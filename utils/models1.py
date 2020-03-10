4a91b4f081bbf329af39c97c3cacbdd56e5fba74
peter6q <luk10099@connect.hku.hk>
2020-03-10 23:38:08 +0800

    Training at 2020-03-10 night

--- utils/models1.py
+++ utils/models1.py
@@ -1,4 +1,5 @@
 import math
+import random
 import torch.nn as nn
 from os.path import join, dirname, isfile
 import torch
@@ -7,7 +8,7 @@ import numpy as np
 from torchvision.models.resnet import resnet34
 import tqdm
 
-DEBUG = 1
+DEBUG = 0
 
 if DEBUG>0:
     import matplotlib.pyplot as plt
@@ -93,32 +94,39 @@ class QuantizationLayer(nn.Module):
                                       activation=activation,
                                       num_channels=dim[0])
         self.dim = dim
-        vox_dim = [ i//2 for i in self.dim]
-        self.vox_dim = tuple(vox_dim)
+        # vox_dim = [ i//2 for i in self.dim]
+        self.vox_dim = tuple(self.dim)
 
     def forward(self, events):
         # points is a list, since events can have any size
         B = int((1+events[-1,-1]).item())
 
         # separate events into S segments with evently-divided timestamps
+        # choose M random segments as input channel
         S = 16
+        M = 4
+
 
         # obtain the height & width
         H, W = self.dim
 
         num_diluted = int( np.prod(self.dim) * S * B)
-        dilution = torch.zeros(num_diluted, dtype=torch.int32, device=events.device)
-
         num_container = int( np.prod(self.dim) * B)
-        container = torch.zeros(num_container, dtype=torch.int32, device=events.device)
-        
-        best_dilution = torch.zeros(num_container, dtype=torch.float32, device=events.device)
-        best_dilution = best_dilution.view(-1, H, W)
+        num_combiner = int( np.prod(self.dim) * M * B)
+        # num_counter = int( np.prod(self.dim) * B)
 
-        num_counter = int( np.prod(self.vox_dim) * B)
-        counter = torch.zeros(num_counter, dtype=torch.int32, device=events.device)
+        dilution = torch.zeros(num_diluted, dtype=torch.bool, device=events.device)
 
-        timer = torch.zeros(num_counter, dtype=torch.float32, device=events.device)
+        concentrate = torch.zeros(num_container, dtype=torch.int32, device=events.device)
+        concentrate = concentrate.view(-1, H, W)
+
+        combiner = torch.zeros(num_combiner, dtype=torch.bool, device=events.device)
+
+        # container = torch.zeros(num_container, dtype=torch.int32, device=events.device)
+
+        # counter = torch.zeros(num_counter, dtype=torch.int32, device=events.device)
+
+        # timer = torch.zeros(num_counter, dtype=torch.float32, device=events.device)
 
         # get values for each channel
         # x, y in the form of +x axis with -y axis
@@ -131,74 +139,117 @@ class QuantizationLayer(nn.Module):
         p = (p+1)/2  # maps polarity to 0, 1
 
         row_cnt = len(events)
-        num_events_ones = torch.ones(row_cnt, dtype=torch.int32, device=events.device)
+        num_events_ones = torch.ones(row_cnt, dtype=torch.bool, device=events.device)
         time_separator = ( t / (1/S)).int()
         time_separator[time_separator == S] = S-1
         idx_in_dilution = x + W*y + W*H*time_separator + W*H*S*b
         dilution.put_(idx_in_dilution.long(), num_events_ones, accumulate=False)
-        dilution = dilution.view(-1, S, H, W).float()
-        dilution = dilution.permute(1,0,2,3)
+        dilution = dilution.view(-1, S, H, W)
+        mixture = dilution.clone().detach().float()
+        mixture = mixture.permute(1,0,2,3)
         erode_w = torch.ones( (B, 1, 3, 3), dtype=torch.float32, device=events.device)
         erode_w[:,:,1,1] = 0
         erode_w /= 8
         for i in range(1, S):
-            dilution[i] *= 0.5 + (S-i)/S
-            dilution[i] += dilution[i-1] * i/S
-            dilution[i] = F.conv2d( dilution[i].unsqueeze(0), erode_w, padding=1, stride=1, groups=B) - 0.25
-            if DEBUG==9:
-                img0 = dilution[i][2].numpy()
-                img0 = img0 / np.max(img0)
-                # img0 = np.where(img0>0, 255, 0)
-                plt.imshow(img0, cmap='gray', vmin=0, vmax=1)
-                plt.show()
-        dilution = dilution.permute(1,0,2,3)
-        limiter = dilution>0
-        limiter = limiter.view(B,S,-1)
-        non_zero = (limiter==0).sum(dim=-1)
-        _, best_idx = non_zero.max(dim=-1)
+            mixture[i] *= 0.5 + (S-i)/S
+            mixture[i] += mixture[i-1] * i/S
+            mixture[i] = F.conv2d( mixture[i].unsqueeze(0), erode_w, padding=1, stride=1, groups=B) - 0.25
+        mixture_bin = mixture > 0
+        for i in range(S):
+            concentrate += mixture_bin[i]
+
+        range_limit = concentrate.view(B,-1)
+        range_limit = range_limit > 0
+        dilution = dilution.view(B,S,-1)
+        combiner = combiner.view(B,M,-1)
+        min_pixel_required = range_limit.sum(dim=-1) // S
+        rand_idxs = random.sample( range(0,S), M)
+        for i in range(M):
+            idx = rand_idxs[i]
+            for bi in range(B):
+                combiner[bi][i] = dilution[bi][idx] & range_limit[bi]
+                cnt = 0
+                limit = S//M
+                while cnt<limit and combiner[bi][i].sum(dim=-1)<min_pixel_required[bi]:
+                    cnt += 1
+                    if idx < M//2:
+                        combiner[bi][i] |= dilution[bi][idx+cnt] & range_limit[bi]
+                    else:
+                        combiner[bi][i] |= dilution[bi][idx-cnt] & range_limit[bi]
+        combiner = combiner.view(-1,M,H,W).float()
+
+        # normalizing pixels to range 0-1
+        concentrate = concentrate.view(-1,1,H,W).float()
         for bi in range(B):
-            i = best_idx[bi]
-            best_dilution[bi] = dilution[bi][i]
+            concentrate[bi] /= concentrate[bi].max()
 
-        idx_in_container = x + W*y + W*H*b
-        # num_events_ones = torch.ones_like(idx_in_container, dtype=torch.int32)
-        container.put_(idx_in_container.long(), num_events_ones, accumulate=True)
-        container = container.view(-1, H, W)
+        if DEBUG==9:
+            dilution = dilution.view(-1,S,H,W)
+            for bi in range(B):
+                visualization = plt.figure()
+                fig0 = visualization.add_subplot(221)
+                fig1 = visualization.add_subplot(222)
+                fig2 = visualization.add_subplot(223)
+                fig3 = visualization.add_subplot(224)
+                img0 = concentrate[bi][0].numpy()
+                # img0 = img0 / np.max(img0)
+                # img0 = np.where(img0>0, 255, 0)
+                img1 = dilution[bi][1].numpy()
+                # img1 = img1 / np.max(img1)
+                # img1 = np.where(img1>0, 1, 0)
+                img2 = dilution[bi][2].numpy()
+                # img2 = img2 / np.max(img2)
+                # img2 = np.where(img1>0, 255, 0)
+                img3 = dilution[bi][3].numpy()
+                # img3 = img3 / np.max(img3)
+                # img3 = np.where(img3>0, 255, 0)
+                fig0.imshow(img0, cmap='gray', vmin=0, vmax=1)
+                fig1.imshow(img1, cmap='gray', vmin=0, vmax=1)
+                fig2.imshow(img2, cmap='gray', vmin=0, vmax=1)
+                fig3.imshow(img3, cmap='gray', vmin=0, vmax=1)
+                plt.show()
+            while True:
+                pass
 
-        idx_in_counter = x//2 + W//2*(y//2) + W*H*b//4
-        counter.put_(idx_in_counter.long(), num_events_ones, accumulate=True)
-        counter = counter.float()
-        timerDivider = counter.clone()
-        timerDivider[timerDivider==0] = 1
+        # idx_in_container = x + W*y + W*H*b
+        # # num_events_ones = torch.ones_like(idx_in_container, dtype=torch.int32)
+        # container.put_(idx_in_container.long(), num_events_ones, accumulate=True)
+        # container = container.view(-1, H, W)
 
-        timer.put_(idx_in_counter.long(), t, accumulate=True)
+        # idx_in_counter = x//2 + W//2*(y//2) + W*H*b//4
+        # counter.put_(idx_in_counter.long(), num_events_ones, accumulate=True)
+        # counter = counter.float()
+        # timerDivider = counter.clone()
+        # timerDivider[timerDivider==0] = 1
 
-        timer = timer/timerDivider
+        # timer.put_(idx_in_counter.long(), t, accumulate=True)
+
+        # timer = timer/timerDivider
         
-        counter = counter.view(-1, H//2, W//2)
-        timer = timer.view(-1, H//2, W//2)
+        # counter = counter.view(-1, H//2, W//2)
+        # timer = timer.view(-1, H//2, W//2)
 
-        diff_y = container[:,::2] - container[:,1::2]
-        diff_y = diff_y[:,:,::2] + diff_y[:,:,1::2]
-        diff_y = diff_y.float()
+        # diff_y = container[:,::2] - container[:,1::2]
+        # diff_y = diff_y[:,:,::2] + diff_y[:,:,1::2]
+        # diff_y = diff_y.float()
 
-        diff_x = container[:,:,::2] - container[:,:,1::2]
-        diff_x = diff_x[:,1::2] + diff_x[:,::2]
-        diff_x = diff_x.float()
+        # diff_x = container[:,:,::2] - container[:,:,1::2]
+        # diff_x = diff_x[:,1::2] + diff_x[:,::2]
+        # diff_x = diff_x.float()
 
-        diff_x = diff_x.unsqueeze(dim=1)
-        diff_y = diff_y.unsqueeze(dim=1)
-        timer = timer.unsqueeze(dim=1)
-        counter = counter.unsqueeze(dim=1)
-        best_dilution = best_dilution.unsqueeze(dim=1)
-        best_dilution = F.interpolate(best_dilution, scale_factor=0.5)
+        # diff_x = diff_x.unsqueeze(dim=1)
+        # diff_y = diff_y.unsqueeze(dim=1)
+        # timer = timer.unsqueeze(dim=1)
+        # counter = counter.unsqueeze(dim=1)
+        # best_dilution = best_dilution.unsqueeze(dim=1)
+        # best_dilution = F.interpolate(best_dilution, scale_factor=0.5)
 
-        vox = torch.cat([diff_x, diff_y, timer, counter, best_dilution], dim=1)
+        vox = torch.cat([concentrate, combiner], dim=1)
 
         if DEBUG==9:
             IMG = 0
             print(vox.size())
-            visualization = plt.figure()
+            # visualization = plt.figure()
             # fig0 = visualization.add_subplot(221)
             # fig1 = visualization.add_subplot(222)
             # fig2 = visualization.add_subplot(223)
@@ -255,7 +306,7 @@ class Classifier(nn.Module):
         self.crop_dimension = crop_dimension
 
         # replace fc layer and first convolutional layer
-        # 5 channels, combined_events_representation & x_vector_representation & y_vector_representation & count_representation & time_representation
+        # 1 channel
         self.classifier.conv1 = nn.Conv2d(5, 64, kernel_size=7, stride=2, padding=3, bias=False)
         self.classifier.fc = nn.Linear(self.classifier.fc.in_features, num_classes)
 
